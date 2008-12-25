66f56a3b381ecbf0d8540ecd8120039d7f1775bc
Michael <fairchild@stimble.net>
2008-12-24 17:32:47 -0800

    refactoring of the javascript sound controls. Ability to pause, and unpause, changing the css class along the way.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,38 +1,61 @@
 $(document).ready(function() {
   
   soundManager.onload = function() {
+    console.log('soundManager.onload has begun');
     // SM2 is ready to go!
-    soundManager.debugMode = true
-    // soundManager.consoleOnly = true;
+    soundManager.debugMode = false;
+    soundManager.consoleOnly = false;
     soundManager.multiShot = false;
     soundManager.playNext = true;
+    
+    play_next_song = function(index){ };  //TODO
     
     $(".playlist li ").each(function(i){
         this.id = this.id + "song_" + i;
          var song_link = $(this).find('a');
+         var song_url = song_link.attr('rel');
          // song_link.attr('rel', 'song_'+i);
          // $(this).contents('a').click(function(){});
+         
+         //iterate thru playlist, creating soundManager objects for all playable links
+         if (soundManager.canPlayURL( song_url) ) {          
+            var mySound = soundManager.createSound({
+              id: 'song_'+i,
+              url: song_link.attr('rel'),
+              volume: 95,
+              playNext: true,
+              consoleOnly: true,
+              // onfinish: play_next_song(i);
+              onfinish: function() {
+                  soundManager.play('song_'+i+1);
+                }
+            });
+            // mySound.play();
+          };
+          
         
          $(this).click(function(){
-           // soundManager.stopAll();
-           $(".playlist li ").each(function(){$(this).removeClass('sm2_playing');});
-           console.log('sadfsdf')
-           
-           console.debug( song_link.attr('rel') );
-           console.log('ff')
-           $(this).addClass('sm2_playing');
-           var mySound = soundManager.createSound({
-             id: 'song_'+i,
-             url: song_link.attr('rel'),
-             volume: 95
-           });
-           mySound.play();
-           
+           soundManager.stopAll();
+            //pause if playing
+           if ($(this).attr('class') == 'sm2_playing'){ 
+             soundManager.pause('song_'+i);
+             $(".playlist li.sm2_playing").each(function(){$(this).removeClass('sm2_playing');});
+             $(this).addClass('sm2_paused');
+             
+             console.debug($(this).attr('class')+' :pause song_'+i);
+           }else {
+              console.debug(i+'-----'+$(this).html());
+              $(this).removeClass('sm2_paused');
+              $(this).addClass('sm2_playing');
+              soundManager.play('song_'+i);
+              console.debug( song_link.html() );
+           };
+          });
+  
          });
          // song_link.bind('click', function () {console.log('clicked a linke'); return false });
          
-         
-      });
+        
       
     
   //   var mySound = soundManager.createSound({
